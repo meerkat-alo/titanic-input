9b1777c3fe00c2ec43a809198a04bdadb389058f
ws.jang <ws.jang@lge.com>
2024-06-11 17:17:48 +0900

    hotfix: read csv recursively

--- asset_input.py
+++ asset_input.py
@@ -5,8 +5,8 @@ import os
 import sys
 from alolib.asset import Asset
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-import pandas as pd 
- 
+import pandas as pd  
+from pathlib import Path
 #--------------------------------------------------------------------------------------------------------------------------
 #    CLASS
 #--------------------------------------------------------------------------------------------------------------------------
@@ -21,20 +21,24 @@ class UserAsset(Asset):
     def run(self):
         # alolib API : self.asset.~~~() 
         base_input_path = self.asset.get_input_path() 
-        sub_dir = os.listdir(base_input_path)
-        assert len(sub_dir) == 1
-        input_path = base_input_path + sub_dir[0] + '/'
-        for idx, input_file in enumerate(os.listdir(input_path)):
-            df = pd.read_csv(input_path + input_file)
+        csv_list = find_csv_files(base_input_path)
+        assert len(csv_list) == 1
+        self.asset.save_info(f'input csv files: {csv_list}')        
+        for idx, input_file in enumerate(csv_list):
+            df = pd.read_csv(input_file)
             self.data[f'dataframe{idx}'] = df
             self.asset.save_info(f"Loaded dataframe{idx}") # info logging
-
+            self.asset.save_info(f'read dataframe from <<< {input_file}')
         self.config['x_columns'] = self.args['x_columns']
         self.config['y_column'] = self.args['y_column']
             
         self.asset.save_data(self.data) # to next asset
         self.asset.save_config(self.config) # to next asset
- 
+
+def find_csv_files(root_dir):
+    root_path = Path(root_dir)
+    return list(root_path.rglob('*.csv'))
+
 #--------------------------------------------------------------------------------------------------------------------------
 #    MAIN
 #--------------------------------------------------------------------------------------------------------------------------
